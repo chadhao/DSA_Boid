2a9ad1035a0ffc6c282dc1916bcef788f24c26d5
Duan Hao <haoduan@outlook.com>
2016-03-23 17:35:26 +1300

    Added files via upload

---
+++ BoidFlock.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.awt.Graphics;
+import java.util.*;
+/**
+ *
+ * @author gpz1505
+ */
+public class BoidFlock {
+    public static int DETECTRADIUS = 10;
+    private int frameWidth;
+    private int frameHeight;
+    private List<Boid> boidList;
+    private Map<Boid, Thread> boidThreads;
+    private static Random RAND = new Random();
+    
+    public BoidFlock() {
+        boidList = new ArrayList<>();
+    }
+    
+    public BoidFlock(int numBoidsToStart, int frameWidth, int frameHeight) {
+        this.frameHeight = frameHeight;
+        this.frameWidth = frameWidth;
+        boidList = new ArrayList<>();
+        boidThreads = new HashMap<>();
+        for (int i = 0; i < numBoidsToStart; i++) {
+            boidList.add(new Boid(this, RAND.nextInt(frameWidth), RAND.nextInt(frameHeight), RAND.nextInt(Boid.MAX_SPEED), RAND.nextFloat()*Boid.MAX_SPEED, frameWidth, frameHeight));
+            boidThreads.put(boidList.get(0), new Thread(boidList.get(i)));
+        }
+        startAllThreads();
+    }
+    
+    private void startAllThreads() {
+        for( Map.Entry<Boid, Thread> e : boidThreads.entrySet() ) {
+            e.getValue().start();
+        }
+    }
+    
+    public void addBoidToFlock() {
+        boidList.add(new Boid(this, RAND.nextInt(frameWidth), RAND.nextInt(frameHeight), RAND.nextInt(Boid.MAX_SPEED), RAND.nextInt(Boid.MAX_SPEED), frameWidth, frameHeight));
+        boidThreads.put(boidList.get(boidList.size()-1), new Thread(boidList.get(boidList.size()-1)));
+        boidThreads.get(boidList.get(boidList.size()-1)).start();
+    }
+    
+    public void removeBoidFromFlock() {
+        int randIndex = RAND.nextInt(boidList.size());
+        boidThreads.get(boidList.get(randIndex)).interrupt();
+        boidThreads.remove(boidList.get(randIndex));
+        boidList.remove(randIndex);
+    }
+    
+    public void destroyAllBoids() {
+        for( Map.Entry<Boid, Thread> e : boidThreads.entrySet() ) {
+            e.getValue().interrupt();
+        }
+        boidThreads.clear();
+        boidList.clear();
+    }
+    
+    public int getNumberOfBoids() {
+        return boidList.size();
+    }
+    
+    private synchronized boolean isNeighbour(Boid from, Boid to) {
+        return Math.pow(from.getxPos()-to.getxPos(), 2)+Math.pow(from.getyPos()-to.getyPos(), 2)<Math.pow(DETECTRADIUS, 2);
+    }
+    
+    public synchronized List<Boid> getNeighbours(Boid boidToTest) {
+        List<Boid> tempList = new ArrayList<>();
+        for( Boid aBoid : boidList ) {
+            if ( isNeighbour(boidToTest, aBoid) ) {
+                tempList.add(aBoid);
+            }
+        }
+        return tempList;
+    }
+    
+    public synchronized void drawAllBoids(Graphics g) {
+        for( Boid aBoid : boidList ) {
+            aBoid.drawShip(g);
+        }
+    }
+}
